d0a97626453407a9cdc71590b92bc5e393f7fce2
Michael Deimel <michael@deimel.de>
2014-03-07 13:55:25 +0100

    fix import

--- db/migrate/20130104091342_create_posts.rb
+++ db/migrate/20130104091342_create_posts.rb
@@ -7,6 +7,7 @@ class CreatePosts < ActiveRecord::Migration
       t.integer  :user_id
       t.text     :media
       t.string   :media_type
+      t.string   :content_type
       t.boolean  :twitter_export,                     :default => true
       t.string   :attachment_file_name
       t.string   :attachment_content_type
