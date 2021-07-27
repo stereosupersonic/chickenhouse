2598a6cd95072b9df5eb899f06e4527294ab91b4
michael <m.deimel@miceportal.com>
2021-07-27 12:42:45 +0200

    assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,5 +1,7 @@
 # Be sure to restart your server when you modify this file.
-
+# Enable the asset pipeline
+Rails.application.config.assets.enabled = true
+Rails.application.config.assets.initialize_on_precompile = false
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = "1.0"
 
@@ -8,6 +10,7 @@ Rails.application.config.assets.version = "1.0"
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join("node_modules")
 
+Rails.application.config.serve_static_assets = true
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
