78b3b3692e36fdc96a731be33db070957310f334
MICE Portal <michael@deimel.de>
2025-01-08 15:22:25 +0100

    add more specs

--- spec/system/welcome_page_spec.rb
+++ spec/system/welcome_page_spec.rb
@@ -15,4 +15,35 @@ describe "welcome" do
     expect(page).to have_link "Impressum"
     expect(page).to have_content "Alle Rechte vorbehalten"
   end
+
+  it "shows a logo when no recent posts are available" do
+    create :post, created_at: 7.months.ago
+
+    visit root_path
+
+    expect(page).to have_css("img[src*='logo_fcb']")
+  end
+
+  it "shows the recent posts but not the log" do
+    create :post, title: "title 1"
+    create :post, title: "title 3"
+    create :post, title: "title 2"
+
+    visit root_path
+
+    expect(page).to have_content "title 1"
+    expect(page).to have_content "title 2"
+    expect(page).to have_content "title 3"
+    expect(page).to_not have_css("img[src*='logo_fcb']")
+  end
+
+  it "shows the recent events but none in the past" do
+    create :event, title: "event 1"
+    create :event, title: "old event", start_date: 1.year.ago
+
+    visit root_path
+
+    expect(page).to have_content "event 1"
+    expect(page).to_not have_content "old event"
+  end
 end
